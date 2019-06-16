fb5574796e90ee6a1ca75059c901b2cbcdc9fc1b
DBinary <matrixcascade@gmail.com>
2019-06-16 21:42:55 +0800

    update some functions

--- PainterEngine/Core/PX_Font.h
+++ PainterEngine/Core/PX_Font.h
@@ -2,10 +2,14 @@
 #define PIXELSES_FONT
 
 #include "../Core/PX_Surface.h"
+#include "../Core/PX_Texture.h"
+#include "PX_Hashmap.h"
 
 #define __PX_FONT_GBKSIZE		13
 #define __PX_FONT_ASCSIZE		8
 #define __PX_FONT_HEIGHT		15
+#define __PX_FONT_MODULE_XSPACE_SIZE 8
+#define __PX_FONT_MODULE_YSPACE_SIZE 8
 ////////////////////////////////////////////////////////////////////////////////////////////
 ///////								Private function									////
 //////																					////	
@@ -15,11 +19,42 @@
 #define PX_FontGetAscCharactorWidth() (__PX_FONT_ASCSIZE)
 #define PX_FontGetGbkCharactorWidth() (__PX_FONT_GBKSIZE)
 
+
+typedef struct
+{
+	union
+	{
+		px_char  c_magic[4];//PXFM
+		px_dword magic;
+	};
+	px_dword charactor_code;
+	px_dword BearingX;
+	px_dword BearingY;
+	px_dword Advance;
+	px_dword Font_Width;
+	px_dword Font_Height;
+	px_shape shape;
+}PX_FontModule_Charactor;
+
+typedef struct  
+{
+	px_memorypool *mp;
+	px_map characters_map;
+	px_int xspacer;
+	px_int yspacer;
+}PX_FontModule;
+
 px_void PX_FontDrawGBK(px_surface *psurface,px_int x,px_int y, px_uchar *str,px_color Color);
 px_void PX_FontDrawASCII(px_surface *psurface,px_int x,px_int y, px_uchar ASCI,px_color Color);
 px_void PX_FontDrawText(px_surface *psurface,int x,int y,px_char *Text,px_color Color);
 px_void PX_FontDrawChar(px_surface *psurface,int x,int y,px_char *Text,px_color Color);
 px_int  PX_GetFontTextPixelsWidth(px_char *Text);
+
+px_bool PX_FontModuleInitialize(px_memorypool *mp,PX_FontModule *module);
+px_bool PX_FontModuleLoad(PX_FontModule *module,px_byte *buffer,px_int size);
+px_void PX_FontModuleFree(PX_FontModule *module);
+px_void PX_FontModuleDrawText(px_surface *psurface,int x,int y,px_char *Text,px_color Color,PX_FontModule *mod);
+px_void PX_FontModuleSetXYSpace(PX_FontModule *module,int x,int y);
 
 #endif
  
--- PainterEngine/Core/PX_Hashmap.h
+++ PainterEngine/Core/PX_Hashmap.h
@@ -26,6 +26,19 @@ typedef struct
 	struct px_rb_root root;
 }px_map;
 
+typedef struct		__px_map_element
+{
+	struct px_rb_node node;//must be first member
+	px_char		string[PX_HASHMAP_STRINGKEY_LEN];
+	union
+	{
+		px_void		*Ptr;
+		px_int      _int;
+		px_float    _float;
+	};
+
+}px_map_element;
+
 
 
 px_bool PX_MapInit(px_memorypool *mp,px_map *hashmap);
@@ -34,6 +47,8 @@ PX_HASHMAP_RETURN PX_MapPut(px_map * m,px_char *stringkey, px_void * value);
 PX_HASHMAP_RETURN PX_MapPutInt(px_map * m,px_char *stringkey, px_int value);
 PX_HASHMAP_RETURN PX_MapPutFloat(px_map * m,px_char *stringkey, px_float value);
 PX_HASHMAP_RETURN PX_MapErase(px_map * m,px_char *stringkey);
+px_map_element *  PX_MapFirst(px_map * m);
+px_map_element *  PX_MapNext(px_map * m,px_map_element *node);
 px_void *PX_MapGet(px_map * m, px_char* stringkey);
 px_bool PX_MapGetInt(px_map * m, px_char* stringkey,px_int *_int);
 px_bool PX_MapGetFloat(px_map * m, px_char* stringkey,px_float *_float);
