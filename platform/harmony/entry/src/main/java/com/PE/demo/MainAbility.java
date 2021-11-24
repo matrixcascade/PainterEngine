5b98b30dd2d6aafeff11b4da72f259c3de769575
破竹 <1249223183@qq.com>
2021-11-24 16:19:33 +0800

    添加鸿蒙opengl_demo

---
+++ platform/harmony/entry/src/main/java/com/PE/demo/MainAbility.java
@@ -0,0 +1,13 @@
+package com.PE.demo;
+
+import com.PE.demo.slice.MainAbilitySlice;
+import ohos.aafwk.ability.Ability;
+import ohos.aafwk.content.Intent;
+
+public class MainAbility extends Ability {
+    @Override
+    public void onStart(Intent intent) {
+        super.onStart(intent);
+        super.setMainRoute(MainAbilitySlice.class.getName());
+    }
+}
